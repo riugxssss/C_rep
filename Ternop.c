970a85a087bd28d53b513644bfcc8c0fba095863
riugxs. <riugxss@gmail.com>
2024-12-22 22:25:13 +0100

    .

--- Ternop.c
+++ Ternop.c
@@ -9,7 +9,7 @@ int main(){
     scanf("%d", &t1);
     //Sum of numbers 
     for (int i=0; i<=t1; printf("%d\n", somma+=i), i++);
-    //Terniar operator to simulate an if-else in a fast and smart way
+    //Ternary operator to simulate an if-else in a fast and smart way
     char *c = (somma >= 15) ? "First face " : "Second face -le 15";
     printf("%s", c);
     return 0;
