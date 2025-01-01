5bbacfc82df1bb0700a53d4a9c31519f526dbce7
riugxs. <riugxss@gmail.com>
2025-01-01 14:24:18 +0100

    exampleclient.c

--- exampleclient.c
+++ exampleclient.c
@@ -1,69 +1,110 @@
+//Client TCP from riugxss
+
 #include <stdio.h>
+#include <stdlib.h>
+#include <unistd.h>
+#include <string.h>
+#include <arpa/inet.h>
+#include <dirent.h>
 #include <sys/socket.h>
 #include <sys/types.h>
-#include <arpa/inet.h>
-#include <unistd.h>
-#include <netinet/in.h>
-#include <string.h>
-#include <stdbool.h>
+#include <fcntl.h>
+#include <wait.h>
+#include <sys/stat.h>
+#define BUFFER 1024
 
+typedef struct 
+{
+    int portForServer;
+    char LOCAL_address[200];
+}info;
 
-#define BUFFER_SIZE 1024
-#define PORT 8080
+int client(info *data){
+    int server_sock;
+    struct sockaddr_in  addr;
+    char buffer[BUFFER];
+    char messageFromClient[BUFFER];
 
-int main(){
-    char mess[BUFFER_SIZE];
-    char buffer[BUFFER_SIZE] = {0};
-    struct sockaddr_in server;
-    int sock;
-    char *ip = "127.0.0.0";
-
-    sock= socket(AF_INET, SOCK_STREAM, 0);
-    if (sock < 0){
-        printf("Error with the socket");
+    server_sock = socket(AF_INET, SOCK_STREAM, 0);
+    if (server_sock != 0){
+        fprintf(stderr, "Error with the server socket\n");
+        free(data);
+        return 1;
+    }
+    addr.sin_family = AF_INET;
+    addr.sin_port = htons(data->portForServer);
+    if (inet_pton(AF_INET, data->LOCAL_address, &addr.sin_addr) != 0){
+        fprintf(stderr, "Error converting the IP address\n");
+        free(data);
         return 1;
     }
 
-    server.sin_family = AF_INET;
-    server.sin_port = htons(PORT);
-    if (inet_pton(AF_INET, ip, &server.sin_addr) == 1){
-        printf("The IP is valid");
-    }
-    else {
-        perror("IP address invalid..");
-        close(sock);
+    if (connect(server_sock, (struct sockaddr*)&addr, sizeof(addr)) != 0){
+        fprintf(stderr, "Error connectin to the IP: %s", data->LOCAL_address);
+        free(data);
         return 1;
     }
 
-    if (connect(sock, (struct sockaddr*)&server, sizeof(server)) < 0){
-        printf("Impossible to connect to the server");
-        close(sock);
-        return 1;
-    }
-    printf("Connected to the server on port: %d", PORT);
+    printf("Connection from: %s | %d", inet_ntoa(addr.sin_addr), ntohs(addr.sin_port));
 
-    while (true){
-        printf("Send a message (exit for quit) " );
-        fgets(mess, sizeof(mess), stdin);
-        mess[strcspn(mess, "\n")] = '\0'
+    size_t bytes_sent;
+    size_t bytes_recv;
+    printf("Send a message to the server (quit for leave)\n");    
+    while (bytes_sent > 0 && bytes_recv > 0){
+        fgets(messageFromClient, sizeof(messageFromClient), stdin);
 
-        if (strcmp(mess, "exit") == 0){
-            printf("Connetions interrupted");
-            break;
+        messageFromClient[strcspn(messageFromClient, "\n")] = '\0';
+        if (strcmp(messageFromClient, "quit") == 0){
+            printf("Exiting..\n");
+            free(data);
+            exit(EXIT_SUCCESS);
         }
-        send(sock, mess, strlen(mess), 0);
-
-        memset(buffer, 0, BUFFER_SIZE);
-        int bytes = recv(sock, buffer, sizeof(buffer) - 1, 0);
-        if (bytes < 0){
-            printf("Connetion closed by the server");
-            break;
+        
+        bytes_sent= send(server_sock, messageFromClient, strlen(messageFromClient), 0);
+        if (bytes_sent < 0){
+            fprintf(stderr, "Error sending the byte\n");
+            free(data);
+            return 1;
         }
-        printf("Server response: %s", buffer);
 
-
+        memset(buffer, 0, BUFFER);
+        bytes_recv = recv(server_sock, buffer, BUFFER, 0);
+        if (bytes_recv < 0){
+            fprintf(stderr, "Error receiving the data from the serv\n");
+            free(data);
+            return 1;
+        }
+        printf("The server response: %s\n", buffer);
     }
-    close(sock);
+
+    close(server_sock); 
+    free(data);
 
     return 0;
+}
+int main(){
+    info *data = malloc(sizeof(info));
+    if (data == NULL){
+        fprintf(stderr, "Error allocating the memory\n");
+        free(data);
+        return 1;
+    }
+    char ipaddr[150];
+    int port;
+    printf("Enter the port to connect: ");
+    scanf("%d", port);
+    if (port < 0 || port > 65535){
+        fprintf(stderr, "Port out of the range (1-65535)\n");
+        return 1;
+    }
+    printf("Enter the IP address: %d");
+    scanf("%s", ipaddr);
+
+    data->portForServer = port;
+    strncpy(data->LOCAL_address, ipaddr, sizeof(ipaddr) +1);
+
+    printf("Press enter to start the client..\n");
+    getchar();
+
+    free(data);
 }
