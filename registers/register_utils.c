7d52aeca7204c92f6ff6b3556db2fb68e4b438b0
riugxs. <riugxss@gmail.com>
2025-06-05 12:55:19 +0200

    .

--- registers/register_utils.c
+++ registers/register_utils.c
@@ -1,9 +1,11 @@
 #include <stdio.h>
 
+//Print the register copy left shift ( / 2 ) if & 1u = 1 else 0
+
 void print_registers(int reg, int totbit){
     for (int i = totbit - 1;i>= 0;i--){
         printf("%d", (reg >> i) & 1u);
-        if (i % 8 == 0){
+        if (i % 8 == 0){ //spacing by 8 
             putchar(' ');
         }
     }
