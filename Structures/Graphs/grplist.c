25381cb6544915d94aebe3a074b3d6270e133992
riugxs. <riugxss@gmail.com>
2025-08-20 14:37:10 +0200

    .

--- Structures/Graphs/grplist.c
+++ Structures/Graphs/grplist.c
@@ -2,79 +2,106 @@
 #include <stdlib.h>
 
 typedef struct node {
-    int v;
-    struct node *next;
+	int v;
+	struct node *next;
 }node;
+
 typedef struct graph {
-    int tot;
-    struct node **list;
-    int isdir;
+	int isdir;
+	int totnum;
+	struct node **adjlist;
 }graph;
 
-node *create_node(int n){   
-    node *newn = malloc(sizeof(*newn));
-    newn->next = NULL;
-    newn->v = n;
-    return newn;
+node *create_node(int v){
+	node *newn = malloc(sizeof(*newn));
+	if (!newn){
+		fprintf(stderr, "[!] Out of memory\n");
+		exit(EXIT_FAILURE);
+	}
+	newn->v = v;
+	newn->next = NULL;
+	return newn;
+}
+graph *create_graph(int tot, int isdirected){
+	graph *ptr = malloc(sizeof(*ptr));
+	if (!ptr){
+		fprintf(stderr, "[!] Out of memory\n");
+		exit(EXIT_FAILURE);
+	}
+	ptr->isdir = isdirected;
+	ptr->totnum = tot;
+	ptr->adjlist = malloc(sizeof(int *) * tot);
+	if (!ptr->adjlist){
+		fprintf(stderr, "[!] Out of memory\n");
+		free(ptr);
+		exit(EXIT_FAILURE);
+	}
+	for (int i = 0;i<tot;i++){
+		ptr->adjlist[i] = NULL;
+	}
+	return ptr;
 }
 
-graph *create_graph(int tot, int isdir){
-    graph *ptr = malloc(sizeof(*ptr));
-    ptr->tot = tot;
-    ptr->isdir = isdir;
-    ptr->list = malloc(sizeof(node *) * tot);
-    for (int i= 0;i<tot;i++){
-        ptr->list[i] = NULL;
-    }
-    return ptr;
-}
-void print_all(graph *ptr){
-    if (!ptr) return;
-    for (int i = 0; i<ptr->tot;i++){
-        node *tmp = ptr->list[i];
-        printf("List [%d]: ", i);
-        while (tmp){
-            printf("%d ", tmp->v);
-            tmp = tmp->next;
-        }
-        putchar('\n');
-    }
-}
+/*
+vertex 0: ptr->adjlist[0] --> [1 | next] --> [2 | next] --> NULL list of his neighbours: 1 2 
+like vertex 0 is adiacent to 1 and 2 if there are undirected (bidirezional) 1 and 2 also are 
+point to vertex 0
+
+vertex 1: ptr->adjlist[1] --> [0 | next] --> NULL
+vertex 2: ptr->adjlist[2] --> [0 | next] --> NULL
+vertex 3: ptr->adjlist[3] --> NULL
+
+our array of ptr -> is an head of the linked list
+*/
 
 void add_edge(graph *ptr, int src, int dst){
-    node *newn = create_node(dst);
-    newn->next = ptr->list[src]; //is the head
-    ptr->list[src] = newn;
-
-    if (!ptr->isdir){
-        newn = create_node(src);
-        newn->next = ptr->list[dst]; //is the head of the linked list
-        ptr->list[dst] = newn;
-    }
+	node *newn = create_node(dst);
+	newn->next = ptr->adjlist[src]; 
+	ptr->adjlist[src] = newn;
+	if (!ptr->isdir){
+		newn = create_node(src);
+		newn->next = ptr->adjlist[dst];
+		ptr->adjlist[dst] = newn;
+	}
 }
 
-void destroy(graph *ptr) {
-    if (!ptr) return;
-    for (int i = 0; i < ptr->tot; i++) {
-        node *tmp = ptr->list[i];
-        while (tmp) {
-            node *next = tmp->next;
-            free(tmp);
-            tmp = next;
-        }
-    }
-    free(ptr->list);
-    free(ptr);
+void print_graph(graph *ptr){
+	if (!ptr) return;
+	printf("Adjacency List: \n");
+	for (int i = 0;i<ptr->totnum;i++){
+		node *tmp = ptr->adjlist[i];
+		printf("%d ->>", i);
+		while (tmp){
+			printf(" %d -> ", tmp->v);
+			tmp = tmp->next;
+		}
+		printf(" NULL\n");
+	}
+	putchar('\n');
+}
+
+void destroy(graph *ptr){
+	if (!ptr) return;
+	for (int i = 0;i<ptr->totnum;i++){
+		node *tmp = ptr->adjlist[i];
+		while (tmp){
+			node *tmp2 = tmp;
+			tmp = tmp->next;
+			free(tmp2);
+		}
+	}
+	free(ptr->adjlist);
+	free(ptr);
+	ptr = NULL;
 }
 
 int main(void){
-    	
-	graph * ptr = create_graph(4, 0);
+
+	graph *ptr = create_graph(3, 0);
 	add_edge(ptr, 0, 1);
 	add_edge(ptr, 0, 2);
-	add_edge(ptr, 0, 3);
 	add_edge(ptr, 1, 2);
-	print_all(ptr);
-    destroy(ptr);
-    return 0;
+	print_graph(ptr);
+	destroy(ptr);
+	return 0;
 }
