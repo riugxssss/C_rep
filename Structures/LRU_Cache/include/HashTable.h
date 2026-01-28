1d878e792ce4367faa4318fc8ebe819bca344417
riugxs. <riugxss@gmail.com>
2026-01-28 19:34:35 +0100

    Add hash table functions and structure definitions

--- Structures/LRU_Cache/include/HashTable.h
+++ Structures/LRU_Cache/include/HashTable.h
@@ -1,14 +1,15 @@
 #pragma once
 #include "double_linked_list.h"
 
-//Opening address Methodology
 
+//Slots informations
 typedef struct slot_info {
     int key;
     node_infos_t *address;
     int status: 2;
 }slot_info_t;
 
+//Hash table informations
 typedef struct hash_table{
     int UsedSlot;
     int MaxSlot; 
@@ -16,8 +17,14 @@ typedef struct hash_table{
 } hash_table_t;
 
 hash_table_t *hash_table_create(int MaxSlot);
+
 void hash_table_delete(hash_table_t *ptr, int key);
+
 void hash_table_insert(hash_table_t *ptr, int key, node_infos_t *NewMRU);
+
 void hash_table_free(hash_table_t *ptr);
+
 node_infos_t *hash_table_search(int key, hash_table_t *ptr);
+
 float hash_table_load_factor(int n, int m);
+
